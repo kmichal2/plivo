973c6bd1719471815cb366150618a2761798d730
kmichal2 <mkovac@oralys.ca>
2015-09-28 14:29:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,16 @@ def speak():
 
     return Response(str(response), mimetype='text/xml')
 
+@app.route("/hello", methods=['GET', 'POST'])
+def hello():
+    """Respond to incoming calls with a simple text message."""
+    text = "Congratulations! You just made a text to speech app on Plivo cloud!"
+    parameters = {'loop': 1, 'language': "en-US", 'voice': "WOMAN"}
+
+    response = plivoxml.Response()
+    response.addSpeak(text, **parameters)
+
+    return Response(str(response), mimetype='text/xml')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
