08888c1b0b6f7ee465223a4c1600762e15762587
kmichal2 <mkovac@oralys.ca>
2015-10-02 15:18:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def call():
         #response.addSpeak("hello "+client)
         #response.addSpeak(auth_id + auth_token + caller_id + box_id + my_url)
         p = plivo.RestAPI(auth_id, auth_token)
-        response = p.add_speak("hello "+client)
+        response = p.speak("hello "+client)
     elif request.method == 'POST':
         #response = plivoxml.Response()
         #response.addSpeak("hello "+client)
