a1b50f8470746223351d8ed1abfb91925c2c7614
kmichal2 <mkovac@oralys.ca>
2015-10-28 11:53:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,11 +97,11 @@ def initdb():
         port=url.port
     )
     cur = conn.cursor()
-    #cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-    #cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
     cur.close()
     
-    #conn.commit()
+    conn.commit()
     conn.close()
     
     #response = p.send_message(params)
