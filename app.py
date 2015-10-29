67be7ac237f1656be3a384ad098c10ae18b07b9a
kmichal2 <mkovac@oralys.ca>
2015-10-29 11:51:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -121,10 +121,9 @@ def writedb():
         port=url.port
     )
     cur = conn.cursor()
-    #SQL = "UPDATE test SET data = '" + text + "' WHERE num = 100;"
+    #cur.execute("UPDATE test SET data = 'abcd' WHERE num = 100;")
     SQL = "UPDATE test SET data = %s WHERE num = 100;"
     data = (""+text+"",)
-    #cur.execute("UPDATE test SET data = 'abcd' WHERE num = 100;")
     cur.execute(SQL, data)
     cur.execute("SELECT * FROM test;")
     response.addSpeak(cur.fetchone())
